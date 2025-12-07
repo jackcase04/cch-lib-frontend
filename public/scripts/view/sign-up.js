9aaefa52acd37951bb78e7e8374cffa4143ef2b5
Maris_Reinkemeyer <mernbk@umsystem.edu>
2025-12-07 09:11:14 -0600

    Added template for sign up submission.

--- public/scripts/view/sign-up.js
+++ public/scripts/view/sign-up.js
@@ -2,6 +2,23 @@ import {API} from '/scripts/api/api.js';
 
 jQuery(document).ready(function() {
 	
+	var signUpForm = jQuery('#sign-up-form'), 
+		fname = jQuery('#fname'), 
+		lname = jQuery('#lname'), 
+		email = jQuery('#email'),  
+		password = jQuery('#pwd'), 
+		confirmPassword = jQuery('#confirm-pwd'); 
+		
+		
+		
+		signUpForm.on('submit', function(e) {
+			e.preventDefault();
+			
+			alert("Submit event heard."); 
+			
+		
+			
+		});
 	
 	
 });
